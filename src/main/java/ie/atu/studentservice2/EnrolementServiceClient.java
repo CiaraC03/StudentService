c9f020ab205c1e4a90605fd6c2afe49dac626cbb
G00402357@atu.ie <g00402357@atu.ie>
2024-10-21 17:40:39 +0100

    Updated changes

--- src/main/java/ie/atu/studentservice2/EnrolementServiceClient.java
+++ src/main/java/ie/atu/studentservice2/EnrolementServiceClient.java
@@ -1,12 +1,13 @@
 package ie.atu.studentservice2;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-@FeignClient(name = "Student-service", url = "http://localhost:8080")
+@FeignClient(name = "Student-service", url = "http://localhost:8081")
 public interface EnrolementServiceClient {
-    @PostMapping("/enrolement")
+    @PostMapping("/enrollment")
     String someDetails(@RequestBody Students students);
 }
--- src/main/java/ie/atu/studentservice2/StudentService.java
+++ src/main/java/ie/atu/studentservice2/StudentService.java
@@ -1,19 +1,33 @@
 package ie.atu.studentservice2;
 
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class StudentService {
 
-    private final StudentsRepository studentsRepository;
 
-    public StudentService(StudentsRepository studentsRepository){
-        this.studentsRepository = studentsRepository;
+
+
+    private List<Students> studentList = new ArrayList<>();
+
+    public List<Students> getStudentList(){
+        return studentList;
+
+    }
+
+    public Students returnStudent(Students students){
+        studentList.add(students);
+        return students;
     }
 
     public String enrolementMessage(Students students){
-        String message = "Thank you" + students.getStudentId() + ", Your enrolement has been completed ";
+        String message = "Thank you" + students.getStudentId() + ", Your enrollment has been completed ";
         return message;
     }
 }
